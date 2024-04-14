7a8165cfd2b64043f1e7783c326cbdaaca8420b5
HNBL <DerKudinov@gmail.com>
2024-04-14 20:39:22 +0300

    lab3_1

---
+++ src/JavaProgram.java
@@ -0,0 +1,20 @@
+class Study {
+
+    private String course;
+
+    //todo
+
+
+    public String printCourse() {
+        return this.course;
+    }
+
+}
+
+class JavaProgram {
+    public static void main(String[] args) {
+
+        //todo
+    }
+
+}
